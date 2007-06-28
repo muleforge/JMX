0fdaf54f36004b9d26ec8133ba8e31429067f5da
merv <merv@3acaecd3-e833-0410-91f2-9561549dd238>
2007-06-28 17:39:44 +0000

    Autoversioning commit:  a non-deltaV client made a change to /trunk/src/main/java/org/mule/providers/jmx/JmxOperationDispatcher.java

---
+++ src/main/java/org/mule/providers/jmx/JmxOperationDispatcher.java
@@ -0,0 +1,44 @@
+package org.mule.providers.jmx;
+
+import org.mule.providers.AbstractMessageDispatcher;
+import org.mule.umo.UMOEvent;
+import org.mule.umo.UMOMessage;
+import org.mule.umo.transformer.TransformerException;
+import org.mule.umo.endpoint.UMOImmutableEndpoint;
+import org.mule.umo.endpoint.UMOEndpointURI;
+import org.mule.impl.MuleMessage;
+
+import javax.management.*;
+import java.io.IOException;
+
+public class JmxOperationDispatcher extends AbstractMessageDispatcher {
+    public JmxOperationDispatcher(UMOImmutableEndpoint endpoint) {
+        super(endpoint);
+    }
+
+    protected void doConnect() throws Exception { }
+    protected void doDisconnect() throws Exception { }
+    protected void doDispose() { }
+
+    protected void doDispatch(UMOEvent event) throws Exception {
+        invoke(event);
+    }
+
+    protected UMOMessage doSend(UMOEvent event) throws Exception {
+        return invoke(event);
+    }
+
+    protected UMOMessage doReceive(long timeout) throws Exception {
+        return invoke(null);
+    }
+
+    private MuleMessage invoke(UMOEvent event) throws InstanceNotFoundException, IOException, ReflectionException, MBeanException, MalformedObjectNameException, IntrospectionException, ClassNotFoundException, TransformerException {
+        JmxConnector c = (JmxConnector) connector;
+        UMOEndpointURI uri = endpoint.getEndpointURI();
+        return new MuleMessage(c.invoke(
+                UriUtils.createObjectName(uri),
+                UriUtils.createOperationName(uri),
+                UriUtils.createParams(uri, event)
+        ));
+    }
+}
